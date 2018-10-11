7f226dccb5170398d6313661a0a51a7657dea331
Ali <khalid.mohamed@student.fh-kiel.de>
2018-10-11 09:48:58 +0200

    Commit #1

---
+++ ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+# first = raw_input("Your name? ")
+# second = raw_input("Your age? ")
+# third = raw_input("Your weight? ")
+
+module, first, second, third = argv
+
+print "The script is called:", module
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Khalid A. Mohamed'
+my_age = 42 # not a lie
+my_height = 77 # inches
+my_weight = 70 # kg
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d killograms heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is ricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
